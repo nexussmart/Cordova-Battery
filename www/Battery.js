0d856b13b4918ede6f78a4fd65a856fc7bd84ffe
Paul Wisdom <paul@flybuy.com>
2015-04-02 13:06:18 -0700

    cb

--- www/Battery.js
+++ www/Battery.js
@@ -2,17 +2,9 @@ var exec = require("cordova/exec");
 module.exports = {
     getLevel: function(success, failure) {
         var self = this;
-        success = success || function() {};
-        failure = failure || function() {};
-
-        var succCallback = function(level) {
-            if(self.level) {
-                success.apply(self, level);
-            }
-        };
 
         exec(
-            succCallback,
+            success || function() {},
             failure || function() {},
             'BatteryPlugin',
             'getLevel',
