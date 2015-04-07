05578f301839394d54f0cf27792ea2f026a6414a
Paul Wisdom <paul@flybuy.com>
2015-04-07 09:28:54 -0700

    Fix IOS Side

--- src/ios/CordovaBattery.m
+++ src/ios/CordovaBattery.m
@@ -7,23 +7,28 @@
 - (void)getLevel:(CDVInvokedUrlCommand*)command
 {
     self.callbackId = command.callbackId;
-
+    
     UIDevice* currentDevice = [UIDevice currentDevice];
     UIDeviceBatteryState currentState = [currentDevice batteryState];
-    float currentLevel = [currentDevice batteryLevel];
+    
+    currentDevice.batteryMonitoringEnabled = YES;
+    
+    float currentLevel = currentDevice.batteryLevel;
     float percent = currentLevel * 100;
-
+    
+    NSLog(@"[ios BatteryLevel] Current Battery Level: %f", currentLevel);
+    
     NSDictionary *jsonObj = [   [NSDictionary alloc]
-                                initWithObjectsAndKeys :
-                                    [NSNumber numberWithInteger: percent], @"batteryPercent",
-                                    nil
-                            ];
+                             initWithObjectsAndKeys :
+                             [NSNumber numberWithInteger: percent], @"batteryPercent",
+                             nil
+                             ];
     CDVPluginResult *pluginResult = [
-                                        CDVPluginResult
-                                        resultWithStatus : CDVCommandStatus_OK
-                                         messageAsDictionary : jsonObj
-                                    ];
-
+                                     CDVPluginResult
+                                     resultWithStatus : CDVCommandStatus_OK
+                                     messageAsDictionary : jsonObj
+                                     ];
+    
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
